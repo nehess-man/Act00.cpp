30598530963dce116ef5b2ef7caf0e10629ba900
nehess-man <yougmail.com>
2024-09-06 10:44:24 +0200

    modif entreprise.cpp et main.cpp

--- main.cpp
+++ main.cpp
@@ -5,17 +5,20 @@
 using namespace std;
 
 int main() {
-    // Création d'une entreprise
-    Entreprise entreprise1("TechCorp", "Paris");
+    Entreprise entreprise1("Service Informatique de la Communauté de Commune", "Ploërmel");
     cout << "Nom de l'entreprise : " << entreprise1.getNomEntreprise() << endl;
     cout << "Ville de l'entreprise : " << entreprise1.getVilleEntreprise() << endl;
 
-    // Création d'une mission
-    Mission mission1(5, "Développement d'une application web", 40, entreprise1);
+    Mission mission1(6,
+        "Réponse aux problématiques clients et modification du système téléphonique de la Communauté de Commune",
+        35,
+        entreprise1);
+
     cout << "Nombre de personnes impliquées : " << mission1.getNbPersonne() << endl;
     cout << "Descriptif de la mission : " << mission1.getDescriptifMission() << endl;
     cout << "Nombre d'heures : " << mission1.getNbHeure() << endl;
     cout << "Entreprise de la mission : " << mission1.getEntrepriseMission().getNomEntreprise() << endl;
+    cout << "Ville de l'entreprise : " << mission1.getEntrepriseMission().getVilleEntreprise() << endl;
 
     return 0;
 }
